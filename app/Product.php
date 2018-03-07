190b007e663b4a62152e48ba29604bb854769b6f
MARIA <maria23millan@gmail.com>
2018-03-07 20:29:20 +0100

    Primera entrega I

---
+++ app/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+
+{
+     protected $fillable = ['code','name','numberOfStocks'];
+
+     public function productSale()
+     {
+         return $this->belongsTo('App\ProductSale');
+     }
+
+    public function provider()
+    {
+        return $this->belongsToMany('App\Provider');
+    }
+
+}
